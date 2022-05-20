29fc13ce7554273f8c23e3c5bca2c65c22e28d2a
Gauri998 <105905263+Gauri998@users.noreply.github.com>
2022-05-20 11:49:14 +0530

    Update and rename form.py to form.html

--- form.py
+++
@@ -1,8 +0,0 @@
-from tkinter import *
-from tkinter import ttk
-window = Tk()
-window.title("Welcome to TutorialsPoint")
-window.geometry('325x250')
-window.configure(background = "gray")
-ttk.Button(window, text="Hello, Tkinter").grid()
-window.mainloop()
